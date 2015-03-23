4ba6324a49f05e39b46539a18202bd3997b4c152
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-03-23 15:21:57 +0800

    Remove some white spaces.

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,6 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # instead of checking the last digit,
     # check if the weighted sum of the digits is a multiple of 10
     nums_a.reverse.zip([1, 2].cycle).reduce(0){ |sum, pair|
